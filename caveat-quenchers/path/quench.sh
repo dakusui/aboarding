e05405cd6162a18b1ddbd59c894329be0bd2fea8
Hiroshi Ukai, Ph.D <dakusui@gmail.com>
2024-02-14 17:48:28 +0900

    Initial commit.

--- caveat-quenchers/path/quench.sh
+++
@@ -1,9 +0,0 @@
-function main() {
-  local _arg="${1}"
-  local _pathrc="${HOME}/.sh/pathrc"
-  mkdir -p "$(dirname "${_pathrc}")"
-  touch "${_pathrc}"
-  echo 'export PATH='"${_arg}"':$PATH' > "$_pathrc{}"
-}
-
-main "${@}"
--- macaboard.sh
+++ macaboard.sh
@@ -1,13 +1,43 @@
 #!/bin/bash -eu
 # Forcibly use a traditional bash for compatibility's sake.
 
+source "$(dirname $0)/lib/init.rc"
+
 function onboard() {
-  /bin/bash -eu bootstrap/bootstrap.sh
-  /opt/homebrew/bin/bash -eu tools/install-tools.sh
+  # Uninstall bootstrap level tools
+  /bin/bash -eu tools/bootstrap.sh install
+  # Install packages
+  source <(/opt/homebrew/bin/brew shellenv)  
+  /opt/homebrew/bin/bash -eu tools/install-packages.sh private:install
+  /opt/homebrew/bin/bash -eu tools/configurator.sh base org team private
 }
 
 function offboard() {
-
-  /bin/bash -eu bootstrap/bootstrap.sh uninstall
+  # Uninstall packages
+  source <(/opt/homebrew/bin/brew shellenv)  
+  /opt/homebrew/bin/bash -eu tools/install-packages.sh private:uninstall
+  # Uninstall bootstrap level tools
+  /bin/bash -eu tools/bootstrap.sh uninstall
 }
+
+function main() {
+  local _i
+  if [[ "$#" == 0 ]]; then
+    main onboard
+  fi  
+  for _i in $@; do
+    if [[ "${_i}" == onboard ]]; then
+      onboard
+    elif [[ "${_i}" == offboard ]]; then
+      offboard
+    else
+      echo "Unknown subcommand: '${_i}' was given." >&2
+      exit 1
+    fi
+  done
+}
+
+ensure_being_able_to_do_sudo
+main "$@"
+
 
